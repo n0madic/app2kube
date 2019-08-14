c3fc42323dcd16df9c199c5ffb7ac563b66bbe12
Alexander Sheiko <alexander.sheiko@mgid.com>
2019-08-14 11:33:46 +0300

    Refactor encrypt

--- pkg/cmd/encrypt.go
+++ pkg/cmd/encrypt.go
@@ -1,16 +1,18 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strconv"
 	"strings"
 
 	"github.com/n0madic/app2kube"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
 )
 
-const dictKey = "secrets"
+var encryptString string
 
 // NewCmdEncrypt return encrypt command
 func NewCmdEncrypt() *cobra.Command {
@@ -20,55 +22,90 @@ func NewCmdEncrypt() *cobra.Command {
 		Long:  "Encrypts values in secrets section for specified YAML files. The result is written to the same file.",
 		RunE:  encrypt,
 	}
+	encryptCmd.Flags().StringVarP(&encryptString, "string", "", "", "Encrypt the specified string")
 	encryptCmd.Flags().VarP(&valueFiles, "values", "f", "Specify values in a YAML file or a URL (can specify multiple)")
 	return encryptCmd
 }
 
 func encrypt(cmd *cobra.Command, args []string) error {
-	if len(valueFiles) == 0 {
-		return fmt.Errorf("need to specify yaml files")
-	}
-
 	password, err := app2kube.GetPassword()
 	if err != nil {
 		return err
 	}
 
+	if encryptString != "" {
+		encrypted, err := app2kube.EncryptAES(password, encryptString)
+		if err != nil {
+			return err
+		}
+		fmt.Println(app2kube.CryptPrefix + encrypted)
+	} else if len(valueFiles) == 0 {
+		return fmt.Errorf("need to specify yaml files")
+	}
+
 	for _, filePath := range valueFiles {
-		m := make(map[interface{}]interface{})
-		modified := false
+		var modified bool
+		var newYAML string
 
-		yamlFile, err := ioutil.ReadFile(filePath)
+		yamlFile, err := os.Open(filePath)
 		if err != nil {
-			return fmt.Errorf("File read error: %v", err)
+			return fmt.Errorf("File open error: %v", err)
 		}
+		defer yamlFile.Close()
 
-		err = yaml.Unmarshal(yamlFile, &m)
-		if err != nil {
-			return fmt.Errorf("Unmarshal %v", err)
-		}
-
-		for k, v := range m[dictKey].(map[interface{}]interface{}) {
-			value := v.(string)
-			if !strings.HasPrefix(value, app2kube.CryptPrefix) {
-				encrypted, err := app2kube.EncryptAES(password, value)
-				if err != nil {
-					return err
+		scanner := bufio.NewScanner(yamlFile)
+		for scanner.Scan() {
+			// found secrets section
+			if scanner.Text() == "secrets:" {
+				newYAML += scanner.Text() + "\n"
+				// scan secrets
+				for scanner.Scan() {
+					// skip template lines
+					if strings.HasPrefix(strings.TrimSpace(scanner.Text()), "{{") {
+						newYAML += scanner.Text() + "\n"
+						continue
+					}
+					// process line with secret
+					if strings.HasPrefix(scanner.Text(), " ") || strings.HasPrefix(scanner.Text(), "\t") {
+						v := strings.SplitN(scanner.Text(), ":", 2)
+						if len(v) == 2 {
+							// unquote value if necessary
+							stripped := strings.TrimSpace(v[1])
+							value, err := strconv.Unquote(stripped)
+							if err != nil {
+								value = stripped
+							}
+							// encrypt value
+							if !strings.HasPrefix(value, app2kube.CryptPrefix) {
+								encrypted, err := app2kube.EncryptAES(password, value)
+								if err != nil {
+									return err
+								}
+								value = app2kube.CryptPrefix + encrypted
+								modified = true
+							}
+							newYAML += fmt.Sprintf("%s: %s\n", v[0], value)
+						} else {
+							newYAML += scanner.Text() + "\n"
+						}
+					} else {
+						newYAML += scanner.Text() + "\n"
+						break // if a new section begins
+					}
 				}
-				m[dictKey].(map[interface{}]interface{})[k] = app2kube.CryptPrefix + encrypted
-				modified = true
+			} else {
+				newYAML += scanner.Text() + "\n"
 			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			return err
 		}
 
 		if modified {
-			y, err := yaml.Marshal(m)
+			err = ioutil.WriteFile(filePath, []byte(newYAML), 0640)
 			if err != nil {
-				return err
-			}
-
-			err = ioutil.WriteFile(filePath, y, 0640)
-			if err != nil {
-				return fmt.Errorf("File read error: %v", err)
+				return fmt.Errorf("File write error: %v", err)
 			}
 		}
 	}
